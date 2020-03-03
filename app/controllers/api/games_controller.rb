4cd7b5ad2e18e9f3532b8d42a23a6e0f15d8b835
Peter Jang <peter.x.jang@gmail.com>
2020-03-02 18:26:47 -0600

    Add fortune action

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,2 +1,11 @@
 class Api::GamesController < ApplicationController
+  def fortune_action
+    fortunes = [
+      "You will go to Arby's in 3 days.",
+      "You will find 20 dollars.",
+      "You will meet an unexpected friend for lunch.",
+    ]
+    @fortune = fortunes.sample
+    render "fortune.json.jb"
+  end
 end
